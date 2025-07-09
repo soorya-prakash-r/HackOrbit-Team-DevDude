a89b05feb9a776da814faf612b9d9fdbe0d71445
Soorya Prakash R <144594996+soorya-prakash-r@users.noreply.github.com>
2025-07-09 17:04:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 
 app.use(cors({
-  origin: 'http://localhost:5173',  
+  origin: process.env.FRONTEND_URL_NEW,  
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'],
 }))
